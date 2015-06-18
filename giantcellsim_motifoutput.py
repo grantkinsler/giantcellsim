85faf72ff4a09c7104f2fd5d16e80d238478d2f4
kins912 <kins912@gmail.com>
2015-06-18 10:22:13 -0400

    added flatten, fixed error in aggregate lists

--- giantcellsim_motifoutput.py
+++ giantcellsim_motifoutput.py
@@ -3,6 +3,12 @@ from giantcellsim_trial import giantcellsim_trial
 import itertools
 import numpy
 
+def flatten(items, seqtypes=(list, tuple)): # used for flattening lists
+    for i, x in enumerate(items):
+        while isinstance(items[i], seqtypes):
+            items[i:i+1] = items[i]
+    return items
+
 
 def giantcellsim_motifoutput(parameterlist,masterprefix,testprefix,trials,growthIterations,max_strand_nr,maxStrandLength,numCells,numRounds,motif,elong,bias):
 	pop_tracker = []
@@ -33,7 +39,13 @@ def giantcellsim_motifoutput(parameterlist,masterprefix,testprefix,trials,growth
 				strands_freq_aggregate = [list(round_data) for round_data in zip(strands_freq_aggregate,strands_freq)]
 				cells_with_freq_aggregate = [list(round_data) for round_data in zip(cells_with_freq_aggregate,cells_with_freq)]
 				nr_strands_per_time = [list(round_data) for round_data in zip(nr_strands_per_time,nr_strands)]
-		
+
+		for time_point in range(numRounds):
+			motif_freq_aggregate[time_point] = flatten(motif_freq_aggregate[time_point])
+			strands_freq_aggregate[time_point] = flatten(strands_freq_aggregate[time_point])
+			cells_with_freq_aggregate[time_point] = flatten(cells_with_freq_aggregate[time_point])
+			nr_strands_per_time[time_point] = flatten(nr_strands_per_time[time_point])
+
 		means = []
 		stdevs = [] 
 
@@ -42,12 +54,12 @@ def giantcellsim_motifoutput(parameterlist,masterprefix,testprefix,trials,growth
 			stdevs.append([])
 
 		for time_point in range(numRounds):
-			means[0].append(numpy.mean(numpy.asarray(motif_freq_aggregate[time_point])))
-			stdevs[0].append(numpy.std(numpy.asarray(motif_freq_aggregate[time_point])))
-			means[1].append(numpy.mean(numpy.asarray(strands_freq_aggregate[time_point])))
-			stdevs[1].append(numpy.std(numpy.asarray(strands_freq_aggregate[time_point])))
-			means[2].append(numpy.mean(numpy.asarray(cells_with_freq_aggregate[time_point])))
-			stdevs[2].append(numpy.std(numpy.asarray(cells_with_freq_aggregate[time_point])))
+			means[0].append(numpy.mean(motif_freq_aggregate[time_point]))
+			stdevs[0].append(numpy.std(motif_freq_aggregate[time_point]))
+			means[1].append(numpy.mean(strands_freq_aggregate[time_point]))
+			stdevs[1].append(numpy.std(strands_freq_aggregate[time_point]))
+			means[2].append(numpy.mean(cells_with_freq_aggregate[time_point]))
+			stdevs[2].append(numpy.std(cells_with_freq_aggregate[time_point]))
 
 		for mean_data in means:
 			writer.writerow(mean_data)
